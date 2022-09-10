a3d11dfb2c3e48cca37fb7659045aa5e63e3315b
Jamie Cupper <jamiec79@protonmail.com>
2022-09-11 00:34:56 +0100

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
+    <Layout pageTitle="Spookies NFT">
       <p>Spookies is a collection of 8888 unique Ghost NFTs — super mega cute, creative digital collectibles invading the Ethereum blockchain - join us!</p>
       <p>Get access to members-only benefits such as early minting, airdrops, giveaways and many more!</p>
       <p>Go get your own Spooky!</p>
+      <StaticImage
+        alt="Spookies"
+        src="../images/spookies_background.png"
+      />
     </Layout>
   )
 }
