06a7934ae6ea0ce0156d3b9857b0808680fe2b3d
Jamie Cupper <jamiec79@protonmail.com>
2022-09-11 11:28:26 +0100

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,23 +3,6 @@ module.exports = {
     title: "Spookies NFT",
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        defaults: {
-          formats: [`auto`, `webp`],
-          placeholder: `dominantColor`,
-          quality: 100,
-          breakpoints: [750, 1080, 1366, 1920],
-          tracedSVGOptions: {},
-          blurredOptions: {},
-          jpgOptions: {},
-          pngOptions: {},
-          webpOptions: {},
-          avifOptions: {},
-        },
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-image`,
   ],
