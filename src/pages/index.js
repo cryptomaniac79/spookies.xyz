5e5a13858eb6188c86350f2bb5ed1558c4e1b516
Jamie Cupper <jamiec79@protonmail.com>
2022-09-11 21:01:03 +0100

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,9 @@ const IndexPage = () => {
     <StaticImage
     alt="Spookies"
     src="../images/spookies_background.png"
+    width="1500"
+    max-width="auto"
+    height="auto"
     />
       <p>Spookies is a collection of 8888 unique Ghost NFTs — super mega cute, creative digital collectibles invading the Ethereum blockchain - join us!</p>
       <p>Get access to members-only benefits such as early minting, airdrops, giveaways and many more!</p>
